f664fa9fbea214bee3219f26a952456aa37776c5
Alex Huryn <algurin77@gmail.com>
2025-04-27 11:27:06 +0200

    init RTQ Query

---
+++ src/api/apiSlice.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+    reducerPath: 'api',
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
+    endpoints: (builder) => ({
+        getTasks: builder.query({
+            query: () => 'tasks',
+        }),
+        createTask: builder.mutation({
+            query: (newTask) => ({
+                url: 'tasks',
+                method: 'POST',
+                body: newTask,
+            }),
+        }),
+    }),
+});
+
+export const { useGetTasksQuery, useCreateTaskMutation } = apiSlice;
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todosReducer from '../slices/todosSlice';
+import { apiSlice } from '../api/apiSlice';
 
 export const store = configureStore({
     reducer: {
-        todos: todosReducer
-    }
-})
+        todos: todosReducer,
+        [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(apiSlice.middleware),
+});
